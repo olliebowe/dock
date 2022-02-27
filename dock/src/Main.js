f6b93763c4caee6325bf7458b8bce4c66186ae38
Cameron <cambutchart@protonmail.com>
2022-02-27 17:28:23 +1100

    Title added

---
+++ dock/src/Main.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom"
+import { Component } from "react/cjs/react.production.min";
+import Title from "./Title";
+
+class Main extends Component{
+    render(){
+        return(
+            <div>
+                <Title title="Test"/>
+                
+
+            </div>
+        )
+    }
+}
+
+
+
+export default Main;
---
+++ dock/src/Title.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom"
+import { Component } from "react/cjs/react.production.min";
+
+function Title(props){
+    return(
+        <h1>{props.title}</h1>
+    )
+}
+
+
+
+export default Title;
